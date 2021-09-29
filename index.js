489bb59cf95ed6af00592474096457165a7d4749
Freelancer Ahsan <81156921+Ahsan5313@users.noreply.github.com>
2021-09-29 17:32:58 +0600

    add javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+const {getName} = require('./student')
+
+const {getAge} = require('./student')
+
+const {cgpa} = require('./student')
+
+console.log(getName())
+
+
+console.log(getAge())
+
+console.log(cgpa)
---
+++ student.js
@@ -0,0 +1,30 @@
+exports.getName = () =>{
+
+    return 'Ahsanul Huq'
+}
+
+exports.getAge = () => {
+
+
+    return "21"
+}
+
+exports.cgpa = 2.71
+
+
+
+
+
+// exports.rslt = cjap
+
+// exports.getName = getName
+// exports.getAge = getAge
+
+
+// module.exports = {
+
+
+//     getName,
+//     getAge,
+//     cgpa,
+// }
